66280aa506f88c567bc7b05eea4514e8f5a3639b
Noah <70314622+NoahTheNerd@users.noreply.github.com>
2024-06-07 12:07:11 +0200

    Starting with the CommandQueue

---
+++ src/main/kotlin/live/ixnoah/tapactions/commands/CommandQueue.kt
@@ -0,0 +1,18 @@
+import net.minecraft.client.Minecraft
+
+object CommandQueue {
+    private var queue = mutableListOf<String>()
+    
+    fun pushCommand(command: String) {
+        queue.add(command)
+    }
+    
+    fun runNextCommand() {
+        if (queue.size == 0) return
+        
+        val command = queue.first()
+        queue.drop(1)
+
+        Minecraft.getMinecraft().thePlayer.sendChatMessage("/" + command)
+    }
+}
